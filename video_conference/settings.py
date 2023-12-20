44ee65653f7b34ec140820c4b12b3a9c2105f3dd
kassaab <abrhamkassa12@gmail.com>
2023-12-20 13:02:10 -0800

    complete

--- video_conference/settings.py
+++ video_conference/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2ucl9p2+((mwu&5)+qq$8yd^=w52tmhd=fmok=+ej+qxwr==_b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
